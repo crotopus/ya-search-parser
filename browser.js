c4a4f7b37c978a26aa1978cd5200e7dd97997918
Temich <61833701+reson-it@users.noreply.github.com>
2020-03-25 18:57:31 +0300

    Новая логика работы
    Сначала забираем просмотры
    Если они есть, забираем количество результатов
    Если есть результаты, забираем выдачу
    
    Co-Authored-By: Nikita Kiose <crotopus@yandex.ru>

--- browser.js
+++ browser.js
@@ -7,67 +7,66 @@ const path = require('path')
 const fs = require('fs')
 
 const webview = document.querySelector('webview')
-const addressbar = document.getElementById('addressbar')
 const queryinput = document.getElementById('queryinput')
 const go = document.getElementById('go')
+const pause = document.getElementById('pause')
+const addQuery = document.getElementById('addQuery')
 const page = document.getElementById('page')
 const resultslimit = document.getElementById('resultslimit')
 
-let currentq              // Строка с текущим запросом
-resultslimit.value = 20   // Максимальная страница
+let currentq                                 // Строка с текущим запросом
+resultslimit.value = 15                      // Максимальная страница
+const searchInterval = 1000 * 2              // Интервал опроса сервера при работа
+const searchIntervalWaiting = 1000 * 60 * 60 // Интервал опроса сервера при простое
 
 const ya = 'https://www.yandex.ru/'
 const search = 'search/?lr=213&text='
 
 let data = []
 let resNum = 0
+let viewsNum = 0
 
 let isSearching
+let onPause
 
 webview.addEventListener('ipc-message', (event) => {
     if (Number(page.value) < Number(resultslimit.value) && currentq != undefined) {
         if (event.channel == 'emptyPage') {
             
+        } else if (event.channel == 'noPositions') {
+            console.log('no positions')
         } else if (event.channel == 'savePage') {
             data = data.concat(event.args[0])
             page.value = Number(page.value) + 1
+            webview.loadURL(ya + search + currentq + '&p=' + page.value)
+        } else if (event.channel == 'saveViewsNum') {
+            viewsNum = event.args[0]
+            webview.send('getResNum')
+            console.log('views num recieved ', viewsNum)
         } else if (event.channel == 'saveResNum') {
             resNum = event.args[0]
+            isSearching = true
+            webview.send('search')
+            console.log('res num recieved ', resNum)
+        } else if (event.channel == 'noViewsNum') {
+            sendEmptyQuery()
         }
-        webview.loadURL(ya + search + currentq + '&p=' + page.value)
     } else {
         console.log(data)
         isSearching = false
         page.value = 0
-        // отправить данные на сервер
-        if (data != []) {
-            request.post('http://localhost:8000/results', {
-                json: {
-                    queryName: currentq,
-                    date: getCurrentDate(),
-                    results: resNum,
-                    urls: data
-                }
-            },
-            (error, res, body) => {
-                if (error) {
-                    console.log(error)
-                    return
-                }
-                console.log(`statusCode: ${res.statusCode}`)
-                console.log(body)
-            })
-        }        
+        sendResults()
         data = []
         resNum = 0
         currentq = null
+        setTimeout(startSearch, searchInterval)
     }
 })
 
 webview.addEventListener('dom-ready', (event) => {
     webview.openDevTools()
     if (page.value == 0) {
-        webview.send('getResultsNumber')
+        webview.send('getViewsNum')
     }
     if (isSearching) {
         webview.send('search')
@@ -78,34 +77,115 @@ getCurrentDate = () => {
     return Number(new Date())
 }
 
-startSearch = () => {
-    isSearching = true
-
-    // Захват запроса из строки
-    //currentq = queryinput.value
-
-    request.get('http://localhost:8000/results/24', {}, 
+getQueryLatest = (toRun) => {
+    request.get('http://localhost:8000/query/latest', {}, 
     (error, res, body) => {
         if (error) {
             console.log(error)
             return
         }
         console.log(`statusCode: ${res.statusCode}`)
+        toRun(body)
+    })
+}
+
+startSearch = () => {
+    page.value = 0
+
+    getQueryLatest((body) => {
         if (!body.includes('{"err"')) {
             currentq = body
             webview.loadURL(ya + search + currentq)
+        } else if (!onPause) {
+            setTimeout(startSearch, searchInterval)
         }
     })
 }
 
+sendQuery = () => {
+    let jsonObj = []
+    const queries = queryinput.value.split('\n')
+    queries.forEach(q => {
+        jsonObj.push({name: q.trim()})
+    })
+    queryinput.value = ''
+    request.post('http://localhost:8000/queries', {json: jsonObj},
+    (error, res, body) => {
+        if (error) {
+            console.log(error)
+            return
+        }
+        console.log(`statusCode: ${res.statusCode}`)
+    })
+}
+
+sendEmptyQuery = () => {
+    
+}
+
+sendResults = () => {
+    if (data != []) {
+        request.post('http://localhost:8000/results', {
+            json: {
+                queryName: currentq,
+                date: getCurrentDate(),
+                results: resNum,
+                urls: data
+            }
+        },
+        (error, res, body) => {
+            if (error) {
+                console.log(error)
+                return
+            }
+            console.log(`statusCode: ${res.statusCode}`)
+            console.log(body)
+        })
+    }
+}
+
+stopSearch = () => {
+    onPause = true
+}
+
+/*
+startWordstat = () => {
+    isWordstating = true
+    getQueryLatest((body) => {
+        if (!body.includes('{"err"')) {
+            currentq = body
+            webview.loadURL(ws + words + currentq)
+        } else {
+            setTimeout(startWordstat, searchInterval)
+        }
+    })
+}
+*/
+
+/*
 queryinput.onkeyup = (ev) => {
     if (ev.key == 'Enter') {
         startSearch()
     }
 }
+*/
 
 go.onclick = () => {
     startSearch()
+}
+
+/*
+wordstat.onclick = () => {
+    startWordstat()
+}
+*/
+
+pause.onclick = () => {
+    stopSearch()
+}
+
+addQuery.onclick = () => {
+    sendQuery()
 }
 
 exportToCsv = () => {
--- inject.js
+++ inject.js
@@ -22,31 +22,49 @@ ipcRenderer.on('search', (event) => {
 
     // Сохраняем все позиции
     if (res.length > 0) {
-        console.log('res sent')
         ipcRenderer.sendToHost('savePage', res)
     } else {
-        console.log('err')
+        ipcRenderer.sendToHost('noPositions', res)
     }
 
     function truncator(url) {
         return url.replace('https','')
-                .replace('http','')
-                .replace('://','')
-                .replace('www.','')
+                  .replace('http','')
+                  .replace('://','')
+                  .replace('www.','')
     }
 })
 
-ipcRenderer.on('getResultsNumber', (event) => {
-    const numString = document.querySelector('.serp-adv__found').innerHTML
-    let resNum = Number(numString.replace(/\D/g,''))
-    if (numString.includes('тыс')) {
+ipcRenderer.on('getViewsNum', (event) => {
+    // Забираем количество показов в месяц
+    const viewsNumString = document.querySelector('.serp-adv__displayed').innerText
+    let viewsNum = Number(viewsNumString.replace(/\D/g,''))
+    if (viewsNumString.includes('тыс')) {
+        viewsNum *= 1000
+    } else if (viewsNumString.includes('млн')) {
+        viewsNum *= 1000000
+    }
+    if (viewsNum) {
+        ipcRenderer.sendToHost('saveViewsNum', viewsNum)
+    } else {
+        console.log('no views num')
+        ipcRenderer.sendToHost('noViewsNum')
+    }
+})
+
+ipcRenderer.on('getResNum', (event) => {
+    // Забираем количество результатов выдачи
+    const resNumString = document.querySelector('.serp-adv__found').innerHTML
+    let resNum = Number(resNumString.replace(/\D/g,''))
+    if (resNumString.includes('тыс')) {
         resNum *= 1000
-    } else if (numString.includes('млн')) {
+    } else if (resNumString.includes('млн')) {
         resNum *= 1000000
     }
     if (resNum) {
         ipcRenderer.sendToHost('saveResNum', resNum)
     } else {
         console.log('no res num')
+        ipcRenderer.sendToHost('noResNum')
     }
 })
